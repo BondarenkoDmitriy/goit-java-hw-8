85bfc21c5cbc84e84d06746b4f495997d1c320e0
Bondarenko Dmitriy <89876885+BondarenkoDmitriy@users.noreply.github.com>
2021-10-25 18:07:15 +0300

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,82 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+//        MyArrayList list = new MyArrayList();
+//
+//        list.add(1);
+//        list.add(1);
+//        list.add(2);
+//        list.add(3);
+//        list.add(5);
+//        list.add(8);
+//
+//        list.print();
+//        list.remove(1);
+//        list.print();
+//
+//
+//        System.out.println(list.size());
+//
+//        System.out.println(list.get(1));
+//
+//        list.clear();
+//        list.print();
+// ---------------------------MyArrayList-----------------------------------
+//        MyLinkedList<Integer> myLinkedList = new MyLinkedList<Integer>();
+//        myLinkedList.print();
+//        myLinkedList.add(5);
+//        myLinkedList.print();
+//        myLinkedList.add(6);
+//        myLinkedList.print();
+//        System.out.println(myLinkedList.size());
+//
+//       myLinkedList.remove(1);
+//        myLinkedList.print();
+//
+//       myLinkedList.clear();
+//        myLinkedList.print();
+//        System.out.println(myLinkedList.get(1));
+// ---------------------------MyLinkedList-----------------------------------
+//        MyQueue myQueuelist = new MyQueue();
+//        myQueuelist.add(3);
+//        myQueuelist.add(5);
+//        myQueuelist.add(6);
+//        myQueuelist.print();
+//        myQueuelist.remove(1);
+//        myQueuelist.print();
+//        System.out.println(myQueuelist.size());
+//        System.out.println(myQueuelist.peek());
+//        myQueuelist.print();
+
+// ---------------------------MyQueue-----------------------------------
+// ---------------------------MyStack-----------------------------------
+// ---------------------------MyHashMap-----------------------------------
+
+//        MyHashMap<String, String> myMap = new MyHashMap<>();
+//        myMap.put("A", "B");
+//        myMap.put("B", "B");
+//        myMap.put("C", "B");
+//        myMap.put("D", "B");
+//        myMap.put("E", "B");
+//        myMap.put("F", "B");
+//        myMap.put("G", "B");
+//        myMap.put("H", "B");
+//        myMap.put("I", "B");
+//        myMap.put("J", "B");
+//        myMap.put("K", "B");
+//        myMap.put("L", "B");
+//        myMap.put("M", "B");
+//        myMap.put("N", "B");
+//        myMap.put("O", "B");
+//        myMap.put("P", "B");
+//        myMap.put("Y", "q");
+//        myMap.clear();
+//        myMap.put("Y", "q");
+//        System.out.println();
+//        myMap.printHashMap();
+    }
+}
